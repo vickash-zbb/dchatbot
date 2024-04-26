9d33401149f2065ebb797181aff31f6ead63c3a2
vickash-zbb <vickashshivanzbb@gmail.com>
2024-04-26 15:25:44 +0530

    intrested page fixe

--- client/src/Components/Chatbot/Chatbot.jsx
+++ client/src/Components/Chatbot/Chatbot.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
 
 import { FaPlus } from "react-icons/fa";
 import { IoSend } from "react-icons/io5";
-import logo from '../Assets/logo.png'
-import userimg from '../Assets/you.png'
+import logo from "../Assets/logo.png";
+import userimg from "../Assets/you.png";
 import { IoMdPerson } from "react-icons/io";
 import { CiLogout } from "react-icons/ci";
-import './Chatbot.css'
-import { Link } from 'react-router-dom';
-
+import "./Chatbot.css";
+import { Link } from "react-router-dom";
 
 const Chatbot = () => {
-    return (
-       
-       <>
+  return (
+    <>
       <div className="mainpage row">
         <div className="sidebar col-md-3">
           <div className="upperside">
             <div className="uppersidetop">
-              <img src={logo} className="img-fluid rounded-top logo" alt="logo" />
+              <img
+                src={logo}
+                className="img-fluid rounded-top logo"
+                alt="logo"
+              />
               <span>D-chat</span>
             </div>
             <button className="midbtn">
@@ -28,19 +30,18 @@ const Chatbot = () => {
           </div>
           <div className="lowerside">
             <div className="listitems">
-              <div className="intrestnav">
-                <IoMdPerson />
-                <a href="#" className="intrest-btn">
+              <Link to={"/questions"}>
+                <button className="btn-secondary">
+                  <IoMdPerson />
                   Interest
-                </a>
-              </div>
-             <Link to={'/'}>
-             <button className="btn">
-                <CiLogout />
-                Log out
-              </button>
-             </Link>
-              
+                </button>
+              </Link>
+              <Link to={"/"}>
+                <button className="btn">
+                  <CiLogout />
+                  Log out
+                </button>
+              </Link>
             </div>
           </div>
         </div>
@@ -48,11 +49,11 @@ const Chatbot = () => {
           <div className="chats col-md-10">
             <div className="chat">
               <img src={userimg} alt="" className="usericon" />
-              <p className="txt">Iam in depression</p>
+              <p className="txt">Hello how can i help you</p>
             </div>
             <div className="chat bot">
               <img src={logo} alt="" className="userlogo" />
-              <p className="txt">What’s the reason for your depression?</p>
+              <p className="txt">Past 3 days i lost interest in every think </p>
             </div>
           </div>
           <div className="chatfooter d-flex justify-content-center align-items-center">
@@ -63,13 +64,14 @@ const Chatbot = () => {
               </button>
             </div>
             <p className="mt-3">
-              D-chat may provide inaccurate information about people mental health support.
+              D-chat may provide inaccurate information about people mental
+              health support.
             </p>
           </div>
         </div>
       </div>
     </>
-    );
-}
+  );
+};
 
 export default Chatbot;
